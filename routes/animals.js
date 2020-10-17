5eaf153d9ef8289f3df3397dfa9ce7bdb427d30a
Jared <jaredhight22@gmail.com>
2020-10-17 15:22:39 -0500

    added routes to controllers and basic settings pgs

--- routes/animals.js
+++ routes/animals.js
@@ -13,24 +13,4 @@ router.get('/view-animals-detailed', animalsController.get_view_animals_detailed
 router.get('/view-animals', animalsController.get_view_animals);
 
 
-// router.get('/new-animal-form', function(req, res, next) {
-//     res.render('animals/new-animal-form')
-// });
-
-// router.get('/daily-summary', function(req, res, next) {
-//     res.render('animals/daily-summary')
-// });
-
-// router.get('/edit-animals-form', function(req, res, next) {
-//     res.render('animals/edit-animals-form')
-// });
-
-// router.get('/view-animals-detailed', function(req, res, next) {
-//     res.render('animals/view-animals-detailed')
-// });
-
-// router.get('/view-animals', function(req, res, next) {
-//     res.render('animals/view-animals')
-// });
-
 module.exports = router;
