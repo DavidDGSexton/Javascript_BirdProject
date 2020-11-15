e6b34cf8269be24338d69b463ddbbe2b933c6b4e
David Sexton <Dsexton@gus.pittstate.edu>
2020-11-14 23:09:02 -0600

    Fixing issue where role wasn't added to user

--- config/passport.js
+++ config/passport.js
@@ -34,4 +34,49 @@ module.exports = function (passport) {
             });
         }));
 
+
+        passport.use('local-signup', new LocalStrategy({
+            // by default, local strategy uses username and password, we will override with email
+            usernameField : 'email',
+            passwordField : 'password',
+            passReqToCallback : true // allows us to pass back the entire request to the callback
+        },
+        function(req, email, password, done) {
+    
+            // find a user whose email is the same as the forms email
+            // we are checking to see if the user trying to login already exists
+            User.findOne({ 'email' :  email }, function(err, user) {
+                // if there are any errors, return the error
+                if (err)
+                {
+                    return done(err);
+                }
+                // check to see if theres already a user with that email
+                if (user) {
+                    return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+                } else {
+    
+                    // if there is no user with that email
+                    // create the user
+                    var newUser = new User();
+    
+                    // set the user's local credentials
+                    console.log(req.body.firstName);
+                    console.log(req.body.lastName);
+                    newUser.firstName = req.body.firstName;
+                    newUser.lastName = req.body.lastName;
+                    newUser.email    = email;
+                    newUser.role = req.body.role;
+                    newUser.password = newUser.generateHash(password);
+    
+                    // save the user
+                    newUser.save(function(err) {
+                        if (err)
+                            throw err;
+                        return done(null, newUser);
+                    });
+                }
+            });
+    
+        }));
 };
--- routes/accounts.js
+++ routes/accounts.js
@@ -28,15 +28,4 @@ router.post('/login', passport.authenticate('local-login', {
     failureFlash: true // allow flash messages
 }));
 
-// makes sure a user is logged in
-function isLoggedIn(req, res, next) {
-
-    // if user is authenticated in the session, carry on 
-    if (req.isAuthenticated())
-        return next();
-
-    // if they aren't redirect them to the home page
-    res.redirect('/');
-}
-
 module.exports = router;
