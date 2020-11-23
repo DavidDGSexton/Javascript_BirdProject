101f588c17d9138e26a952c81ac9c36e82cf54db
Jared <jaredhight22@gmail.com>
2020-11-22 18:10:49 -0600

    update feeding page works for some fields

--- controllers/feedings-controller.js
+++ controllers/feedings-controller.js
@@ -30,7 +30,7 @@ exports.post_create_feeding = async function (req, res) {
         comments: req.body.comments,
         weatherConditions: req.body.weatherConditions,
         dateTime: req.body.dateTime,
-        keeperName: req.body.keeperName
+        keeperName: res.locals.user.firstName + ' ' + res.locals.user.lastName,
     });
 
     newFeeding.save(function (err) {
@@ -49,7 +49,6 @@ exports.get_update_feeding = async function (req, res) {
     const medicines = await Medicine.find({});
 
     Feeding.findOne({ _id: req.query.id }, function (err, feeding) {
-       
         if (err) {
             // handle error
         } else {
@@ -60,17 +59,28 @@ exports.get_update_feeding = async function (req, res) {
 };
 
 
-exports.put_update_feeding = function (req, res) {
+exports.put_update_feeding = async function (req, res) {
+
+    // const animal = await Animal.find({enabled: true});
+    // const food = await Food.find({});
+    // const medicine = await Medicine.find({});
+
+    const animal = await Animal.findOne({ _id: req.body.animalId });
+    const food = await Food.findOne({ _id: req.body.foodId });
+    const medicine = await Medicine.findOne({ _id: req.body.medicineId });
+
+    
+
     let enabled = false;
     if (req.body.enabled == 'on') {
         enabled = true;
     }
 
     const updateFeeding = {
-        animalSpecies: req.body.animal.species,
-        animalNickname: req.body.animal.nickname,
-        food: req.body.food,
-        medicine: req.body.medicine,
+        animalSpecies: animal.species,
+        animalNickname: animal.nickname,
+        food: food.name,
+        medicine: medicine.name,
         goalWeightOfAnimal: req.body.goalWeight,
         actualWeightOfAnimal: req.body.actualWeight,
         amountOfFoodFed: req.body.amountOfFoodFed,
@@ -78,15 +88,15 @@ exports.put_update_feeding = function (req, res) {
         comments: req.body.comments,
         weatherConditions: req.body.weatherConditions,
         dateTime: req.body.dateTime,
-        keeperName: req.body.keeperName
+        keeperName: res.locals.user.firstName + ' ' + res.locals.user.lastName
     };
-
-    Feeding.findOneAndUpdate({ _id: req.body.id }, updateFeeding, function (err, data) {
+    
+    Feeding.findOneAndUpdate({ _id: req.body.id }, updateFeeding, function (err) {
         if (err) {
             // handle error
             console.log(err);
         } else {
-            res.redirect('../view-feedings');
+            res.redirect('../feedings/view-feedings');
         }
     });
 };
